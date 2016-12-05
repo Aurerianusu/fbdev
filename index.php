9d7f400b224d8814537e84fff252ee5c3de7e7da
aurelien <aurelien.beranger@gmail.com>
2016-12-05 12:42:39 +0100

    base de travail FB

--- index.php
+++ index.php
@@ -1,4 +1,93 @@
-<html><body><h1>It works good!</h1>
-<p>This is the default web page for this server.</p>
-<p>The web server software is running but no content has been added, yet.</p>
-</body></html>
+<!doctype html>
+<html>
+	<head>
+		<!-- Page Title -->
+	    <title>Concours photo Facebook</title>
+	    
+	    <!-- Meta Tags -->
+	    <meta charset="utf-8">
+	    <meta name="keywords" content="Concours photo Pardon-Maman" />
+	    <meta name="description" content="Participez au concours photo Pardon-maman et tentez de remporter un tattouage gratuit">
+	    <meta name="format-detection" content="telephone=no">
+	    <meta name="author" content="Pardon-Maman">
+	    <meta name="robots" content="noindex,nofollow">
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	    <!-- Theme Styles -->
+	    <link href="/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+	    <link href="/css/style.css" rel="stylesheet" type="text/css">
+	   
+	    <!-- JQUERY LIBRARY -->
+		<script type="text/javascript" src="/js/jquery.min.js"></script>  
+	</head>
+
+	<body>
+		<!-- HEADER -->
+		<header>
+		 	<div class="header-content">
+		        <div class="container">
+		                <div class="logo">
+		                    <a href="https://www.pardon-maman.com">
+		                        <img src="images/splatter.png" height="200" width="275" alt="logo" class="img-responsive">
+		                    </a>
+		                </div>
+		                
+		                <div class="row menu">
+		                    <div class="col-sm-4 col-xs-4">Accueil</div>
+		                    <div class="col-sm-4 col-xs-4">Galerie</div>
+		                    <div class="col-sm-4 col-xs-4">Règles</div>
+		                </div>
+		</header> 
+		<!-- END OF HEADER -->       
+
+		<!-- CONTENT -->
+		<!-- BLOC 1 -->
+		<section id="section-accueil">
+		    <div class="container">
+		        <div class="row text-center">
+		            <div class="col-md-4 col-sm-4 col-xs-12">
+		            	<img src="" alt="" class="">            	
+		            </div>
+		            <div class="col-md-8 col-sm-8 col-xs-12"></div>
+		        </div>
+		        
+		        <div class="row text-right">
+		        	<div class="col-md-12 col-sm-12 col-xs-12"></div>
+				</div>
+		    </div>
+		</section>
+		<!-- END OF CONTENT -->
+
+		<!-- BLOC 1 -->
+		<section id="section-accueil">
+		    <div class="container">
+		        <div class="row text-center">
+		            <div class="col-md-4 col-sm-4 col-xs-12">
+		            	<img src="" alt="" class="">            	
+		            </div>
+		            <div class="col-md-8 col-sm-8 col-xs-12"></div>
+		        </div>
+		        
+		        <div class="row text-right">
+		        	<div class="col-md-12 col-sm-12 col-xs-12"></div>
+				</div>
+		    </div>
+		</section>
+		<!-- END OF CONTENT -->
+
+		<!-- FOOTER -->
+		<footer class="footer">
+		    <div class="container text-center">
+		        <div class="row">
+		            <div class="col-xs-12">
+		                <p>© Copyright 2016. All Rights Reserved by <a target="_blank" href="https://pardon-maman.com"> Pardon-maman</a>.
+		                </p>
+		            </div>
+		        </div>
+		     </div> 
+		</footer>
+		<!-- END OF FOOTER -->
+
+	</body>
+
+</html>
