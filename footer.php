558cdb54e94b2dc7c7a6c5f188b3efa7b2f68372
aurelien <aurelien.beranger@gmail.com>
2016-12-07 14:36:31 +0100

    ajout du footer

---
+++ footer.php
@@ -0,0 +1,31 @@
+<div class="container">
+        <div class="row text-left">
+        	<div class="col-sm-12 col-xs-12">
+               	<a href="./cgu.php">Mentions légales / CGU</a>
+            </div>
+        </div>
+
+        <div class="row text-center">
+        	<div class="col-sm-3 col-xs-6">
+               	<a href="https://www.facebook.com/bootsnipp"><i id="social-fb" class="fa fa-facebook-square fa-3x social"></i></a>
+            </div>
+
+            <div class="col-sm-3 col-xs-6">
+	            <a href="https://twitter.com/bootsnipp"><i id="social-tw" class="fa fa-twitter-square fa-3x social"></i></a>
+	        </div>
+
+	        <div class="col-sm-3 col-xs-6">
+	           	<a href="https://plus.google.com/+Bootsnipp-page"><i id="social-gp" class="fa fa-google-plus-square fa-3x social"></i></a>
+	        </div>
+
+	        <div class="col-sm-3 col-xs-6">
+	            <a href="mailto:bootsnipp@gmail.com"><i id="social-em" class="fa fa-envelope-square fa-3x social"></i></a>
+	        </div>
+		</div>
+
+		<div class="row text-center">
+        	<div class="col-sm-12 col-xs-12">
+               	 <p>© Copyright 2016. All Rights Reserved by <a target="_blank" href="https://pardon-maman.com"> Pardon-maman</a>.
+            </div>
+        </div>
+</div>
