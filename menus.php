a772870017d12d5202788a2ecb05b1876dbbaf01
Guillaume JEAN <screy@icloud.com>
2017-02-06 15:06:20 +0100

    Logo qui renvoie vers la page d'acceuil

--- menus.php
+++ menus.php
@@ -9,10 +9,12 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <span class="navbar-brand">
-                    <img src="public/images/splatter.png" alt="">
-                    <span id="title-brand">Concours<br>Photo</span>
-                </span>
+                <a href="./">
+                    <span class="navbar-brand">
+                        <img src="public/images/splatter.png" alt="">
+                        <span id="title-brand">Concours<br>Photo</span>
+                    </span>
+                </a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
