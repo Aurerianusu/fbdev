c1594d12970ff37a6463dcd39af6f872d5080e95
Guillaume JEAN <screy@icloud.com>
2017-02-06 14:28:10 +0100

    Selection de la photo, affichage en grand

--- participation.php
+++ participation.php
@@ -25,13 +25,13 @@ require_once __DIR__.'/vendor/autoload.php';
 
     $profile_pic = "http://graph.facebook.com/" . $userNode->getId() . "/picture?width=200";
 
-    echo 'Name: ' . $userNode->getName() . '<br>';
-    echo 'User ID ' . $userNode->getId() . '<br>';
-    echo 'Email ' . $userNode->getField('email') . '<br>';
-    echo 'Genre ' . $userNode->getGender() . '<br>';
-    echo 'Anniversaire ' . $userNode->getBirthday()->format('m/d/Y') . '<br>';
-    echo 'Lien du profil  <a href =' . $userNode->getLink() . '>PROFIL FB</a><br>';
-    echo "<img src=\"" . $profile_pic . "\" />";
+    //echo 'Name: ' . $userNode->getName() . '<br>';
+    //echo 'User ID ' . $userNode->getId() . '<br>';
+    //echo 'Email ' . $userNode->getField('email') . '<br>';
+    //echo 'Genre ' . $userNode->getGender() . '<br>';
+    //echo 'Anniversaire ' . $userNode->getBirthday()->format('m/d/Y') . '<br>';
+    //echo 'Lien du profil  <a href =' . $userNode->getLink() . '>PROFIL FB</a><br>';
+    //echo "<img src=\"" . $profile_pic . "\" />";
 
     $photos_request = $fb->get('/me/photos?limit=100&type=uploaded');
     $photos = $photos_request->getGraphEdge();
@@ -78,49 +78,39 @@ require_once __DIR__.'/vendor/autoload.php';
             <div id="main_area">
                 <!-- Slider -->
                 <h1>Galerie Photos </h1>
-                <div class="row">
-                    <div class="col-sm-6" id="slider-thumbs">
-                        <!-- Bottom switcher of slider -->
-                        <ul class="hide-bullets">
-                            <?php
-                                foreach ($all_photos as $key) {
-                                    $photo_request = $fb->get('/'.$key['id'].'?fields=images');
-                                    $photo = $photo_request->getGraphNode()->asArray();
-                                    echo'<li class="col-sm-3 col-xs-2>';
-                                    echo'<a class="thumbnail" id="carousel-selector-0">';
-                                    echo '<img src="'.$photo['images'][1]['source'].'" width=100 class=popular><br>';
-                                    echo'</a>';
-                                    echo '</li>';
-                                }
-                            ?>
-                        </ul>
-
-                        <div class="col-sm-12 col-xs-12 text-center">
-                            <a href="galerie.php" class="btn btn-lg btn-primary send_button">
-                                <span class="glyphicon glyphicon-th"></span>
-                                Voir plus de tatouages...
-                            </a>
+                <form method="post" action="confirmation.php">
+                    <div class="row">
+                        <div class="col-sm-6" id="slider-thumbs">
+                            <!-- Bottom switcher of slider -->
+                            <ul class="hide-bullets">
+                                <?php
+                                    foreach ($all_photos as $key) {
+                                        $photo_request = $fb->get('/'.$key['id'].'?fields=images');
+                                        $photo = $photo_request->getGraphNode()->asArray();
+                                        echo'<li class="col-sm-3 col-xs-2>';
+                                        echo'<a class="thumbnail" >';
+                                        echo '<img  onclick="swap(this)" src="'.$photo['images'][1]['source'].'" class=popular id=popular>';
+                                        echo'</a>';
+                                        echo '</li>';
+                                    }
+                                ?>
+                            </ul>
                         </div>
-
-                    </div>
-                    <div class="col-sm-6">
-                        <div class="col-xs-12" id="slider">
-                            <!-- Top part of the slider -->
-                            <div class="row">
-                                <div class="col-sm-12" id="carousel-bounding-box">
-                                    <div class="carousel slide" id="myCarousel">
-                                        <!-- Carousel items -->
-                                        <div class="carousel-inner">
-                                            <div class="active item" data-slide-number="0">
-                                                <img src="http://placehold.it/235x240&text=zero">
-                                            </div>
-                                        </div>
-                                    </div>
+                        <div class="col-sm-6 ">
+                            <div class="col-xs-12" id="slider">
+                                <div class="carousel-inner ">
+                                    <img id="bigPicture" class="popular">
                                 </div>
+                            </div>
+
+                            <!-- Pour stocker la source de l'image sélectionné -->
+                            <input type="hidden" name="monImage"  id="monImage"  value="" >
+                            <div class="col-sm-12 col-xs-12 text-center">
+                                <input type="submit" class="btn btn-lg btn-primary send_button">
                             </div>
                         </div>
                     </div>
-                </div>
+                </form>
             </div>
         </div>
     </section>
@@ -129,6 +119,11 @@ require_once __DIR__.'/vendor/autoload.php';
     <!-- END OF FOOTER -->
     <footer><?php require 'footer.php' ?></footer>
     <!-- END OF FOOTER -->
-
+    <script>
+        function swap(image) {
+            document.getElementById("bigPicture").src = image.src;
+            document.getElementById("monImage").value = image.src;
+        }
+    </script>
     </body>
 </html>
